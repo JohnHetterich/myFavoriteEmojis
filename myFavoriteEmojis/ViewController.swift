b36c2b264b496b7903ee8d79736842a165b4757e
John Hetterich <hettjohn@badinhs.org>
2016-10-03 08:25:12 -0400

    second commit

--- myFavoriteEmojis/ViewController.swift
+++ myFavoriteEmojis/ViewController.swift
@@ -8,18 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var coolTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        coolTableView.dataSource = self //used to define the table
+        coolTableView.delegate = self //used to define contents
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //number of rows
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //what do you want in the cells
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Hello 😎"
+        return cell
+    }
 
 }
 
